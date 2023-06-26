593853b93688458bc17e2013516ac0b8191c7e80
Thomas Dupre <thomasdpr-pro@outlook.com>
2023-06-26 17:56:54 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,23 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
+  resources :users, only: [:show] # Pour afficher le profil d'un utilisateur
+
   resources :cars, only: %i[index show] do
-    resources :bookings, only: [:create]
+    resources :bookings, only: [:create] # Pour créer une réservation à partir d'une voiture spécifique
   end
 
-  resources :users, only: [:show] # Pour voir le profil d'un owner
-
-  resources :bookings, only: [:index] # Pour voir les réservations d'un user connecté
+  resources :bookings, only: [:index] # Pour afficher toutes les réservations d'un utilisateur
 
   namespace :owner do
-    resources :bookings, only: [:index] do
+    resources :bookings, only: [:index] do # Pour afficher toutes les réservations d'un propriétaire
       member do
         patch :accept
         patch :decline
+        patch :cancel
       end
     end
-    resources :cars
+
+    resources :cars, except: [:show] # Pour créer, modifier et supprimer une voiture à partir du tableau de bord du propriétaire
   end
 end
