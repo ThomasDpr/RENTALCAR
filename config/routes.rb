a3f4e2d8e7ed38c1fd928815617bd76283e7b315
Thomas Dupre <thomasdpr-pro@outlook.com>
2023-06-26 17:37:16 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'pages#home'
+
+  resources :cars, only: %i[index show] do
+    resources :bookings, only: [:create]
+  end
+
+  resources :users, only: [:show] # Pour voir le profil d'un owner
+
+  resources :bookings, only: [:index] # Pour voir les réservations d'un user connecté
+
+  namespace :owner do
+    resources :bookings, only: [:index] do
+      member do
+        patch :accept
+        patch :decline
+      end
+    end
+    resources :cars
+  end
 end
