3617a5ab2242b261fa5d364af4f207b9bcf747b1
Thomas Dupre <thomasdpr-pro@outlook.com>
2023-06-26 15:59:04 +0200

    Assembling model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :bookings, dependent: :destroy
+  # Un user peut avoir plusieurs bookings (dépendant de lui)
+  # Si on supprime un user, on supprime aussi ses bookings
+
+  has_many :cars, dependent: :destroy
+  # Un user peut avoir plusieurs cars (dépendant de lui)
+  # Si on supprime un user, on supprime aussi ses cars
+
+  has_one_attached :photo
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  # Un user doit avoir un prénom, un nom et un email
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_26_123039) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_26_132636) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "total_price"
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "car_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_bookings_on_car_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "cars", force: :cascade do |t|
+    t.string "brand"
+    t.text "description"
+    t.string "color"
+    t.string "picture"
+    t.integer "price_per_day"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "owner_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_26_123039) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "cars"
+  add_foreign_key "bookings", "users"
 end
